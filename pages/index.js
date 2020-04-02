a450c80af816c0f5c4bbabe0f29cf15bed408cc9
Jeremy Black <jb@yodacom.com>
2020-04-02 13:14:02 -0600

    completed

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,35 @@
-import Head from 'next/head'
+import Link from "next/link";
+import fs from "fs";
 
-const Home = () => (
-  <div className="container">
-    <Head>
+const Home = ({ slugs }) => (
+  <div>
+    slugs:
+    {slugs.map(slug => {
+      return (
+        <div key={slug}>
+          <Link href="/blog/[slug]" as={"/blog/" + slug}>
+            <a> {"/blog/" + slug}</a>
+          </Link>
+        </div>
+      );
+    })}
+  </div>
+);
+
+export const getStaticProps = async () => {
+  const files = fs.readdirSync("posts");
+  return {
+    props: {
+      slugs: files.map(filename => filename.replace(".md", ""))
+    }
+  };
+};
+export default Home;
+
+// This is the default page and CSS for Nextjs:
+
+{
+  /* <Head>
       <title>Create Next App</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
@@ -196,8 +223,5 @@ const Home = () => (
       * {
         box-sizing: border-box;
       }
-    `}</style>
-  </div>
-)
-
-export default Home
+    `}</style> */
+}
---
+++ posts/faq.md
@@ -0,0 +1,23 @@
+---
+title: FAQ
+description: Frequently Asked Questions
+---
+
+## What keybord do you use?
+
+[Das Keyboard with brow switches](https://www.daskkeyboard.com/daskeyboard-4-professional/)
+
+## How do you code so fast?
+
+- Vim: https://www.youtube.com/watchv=IiwGbcd8s7I
+- VSCode shortcuts: https://www.youtube.com/v=t9kSTiqhUfg
+
+## How do you get Autocompletion in your terminal
+
+[Fish Shell](https://fishshell.com)
+
+## What theme do you use?
+
+- VSCode: default dark theme with [bracket-pair-colorizer-2](https://marketplace.visualstudio.com/items?itemName=CoenraadS.bracket-pair-colorizer-2)
+- iTerm2: [gruvbox](https://github.com/morhetz/gruvbox-contrib)
+  If you want to see all my VSCode settings/extensions: https://gist.github.com/benawad/1e9dd01994f78489306fbfd6f7b01cd3
